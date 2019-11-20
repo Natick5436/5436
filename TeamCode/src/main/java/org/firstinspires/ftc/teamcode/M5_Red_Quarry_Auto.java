674dd7a63df510afff66de61e19119114b342292
nopalmer <nopalmer@students.natickps.org>
2019-11-20 09:03:01 -0500

    Added M5_Red_Quarry_Auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/M5_Red_Quarry_Auto.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name = "M5 Red Quarry", group = "Autonomous")
+public class M5_Red_Quarry_Auto extends LinearOpMode {
+
+    Mark_5 robot = new Mark_5(this);
+
+    @Override
+    public void runOpMode(){
+        robot.initialize(hardwareMap);
+
+        waitForStart();
+
+
+    }
+}
