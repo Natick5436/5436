6487ea675c057729d59445e87bd2eddfdcd38288
nopalmer <nopalmer@students.natickps.org>
2019-11-29 17:55:29 -0500

    Added quit times to Build Site autos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/M5_Blue_Build_Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/M5_Blue_Build_Auto.java
@@ -2,11 +2,13 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous(name = "M5 Blue Build Site", group = "Autonomous")
 public class M5_Blue_Build_Auto extends LinearOpMode {
 
     Mark_5 robot = new Mark_5(this);
+    ElapsedTime runtime = new ElapsedTime();
 
     final double FIELD_WIDTH = 3.5814;
     final double ROBOT_WIDTH = 0.4572;
@@ -17,6 +19,17 @@ public class M5_Blue_Build_Auto extends LinearOpMode {
     final double FOUNDATION_WIDTH = 0.4699;
     final double WALL_TO_FOUNDATION = 0.1016;
     final double START_TO_FOUNDATION = 1.20015;
+    final double SKYBRIDGE_LENGTH = 1.168;
+
+    final int ARM_OUT = -2520;
+    final int ARM_MID = -1260;
+    final int ARM_IN = 0;
+
+    final double CLAMP_CLOSE = 0.9;
+    final double CLAMP_OPEN = 0.45;
+
+    final double FLIP_COLLECT = 0.33;
+    final double FLIP_STORE = 1;
 
     final double metersPerMm = 0.001;
 
@@ -24,18 +37,37 @@ public class M5_Blue_Build_Auto extends LinearOpMode {
     final int Y = 1;
     final int Z = 2;
 
+    final double QUIT_TIME = 25;
+    final double QUIT_X = (SKYBRIDGE_LENGTH/4);
+    final double QUIT_Y = FIELD_WIDTH/2;
     @Override
     public void runOpMode(){
         robot.initialize(hardwareMap, ROBOT_WIDTH, FIELD_WIDTH - (WALL_TO_FOUNDATION + FOUNDATION_LENGTH/2), 0);
 
+        runtime.reset();
         waitForStart();
 
         robot.forward(1, START_TO_FOUNDATION-ROBOT_WIDTH);
         robot.setGrab(1);
+        if(runtime.seconds()> QUIT_TIME){
+            robot.setGrab(0);
+            robot.goToAbsolutePosition(1, QUIT_X, QUIT_Y);
+            return;
+        }
         robot.forward(1, -((FOUNDATION_LENGTH/2)-0.05));
+        if(runtime.seconds()> QUIT_TIME){
+            robot.setGrab(0);
+            robot.goToAbsolutePosition(1, QUIT_X, QUIT_Y);
+            return;
+        }
         robot.turn(1, Math.PI/2);
+        if(runtime.seconds()> QUIT_TIME){
+            robot.setGrab(0);
+            robot.goToAbsolutePosition(1, QUIT_X, QUIT_Y);
+            return;
+        }
         robot.forward(1, FOUNDATION_WIDTH);
         robot.setGrab(0);
-        robot.goToAbsolutePosition(1, ROBOT_WIDTH/2+0.03, FIELD_WIDTH/2);
+        robot.goToAbsolutePosition(1, QUIT_X, QUIT_Y);
     }
 }
